6e6c747c346a51cd273bba94aabbf66894186cdb
nicorossi2810 <272179@studenti.unimore.it>
2023-03-01 17:07:42 +0100

    first assignment

--- First_assignment/First_assignment/main.c
+++ First_assignment/First_assignment/main.c
@@ -1,16 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<stdbool.h>
+#include<time.h>
+
+
+void Scambia(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/*void  Bubble(int dim, int vet[]){
+	int i;
+	bool ordinato = false;
+
+	while (dim> 1 && !ordinato)
+	{
+		ordinato = true;
+		for( i=0; i < dim-1;i++ )
+			if(vet[i] > vet[i+1]) {
+				scambia(&vet[i], &vet[i+1]);
+				ordinato = false;
+			}
+		dim--;
+	}
+}*/
 
 int main(int argc, char* argv[]) {
 	//  zona parametri
 	FILE* filein, * fileout;
 	char ch;
 	char* value;
-	int dim_2 = 1, num = 0,dim = 1;
+	int dim_2 = 1, num = 0, dim = 1;
 	int* values;
-	
-	
+	clock_t t1, t2;
+
+	t1 = clock();
 	// controlliamo  che i parametri siano corretti
 	if (argc != 3)
 	{
@@ -28,20 +54,21 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	// allochiamo lo spazio di memoria 
-	if ((value = (char*)malloc(dim) == NULL))
+	if ((value = (char*)malloc(dim_2)) == NULL)
 	{
 		printf("Errore nella allocazione\n");
 		exit(1);
 	}
+	printf("valore di value: %c\n", &value[0]);
 
-	if ((values = (int*)malloc(dim * sizeof(int)) == NULL))
+	if ((values = (int*)malloc(dim * sizeof(int))) == NULL)
 	{
 		printf("Errore nella allocazione\n");
 		exit(1);
 	}
 
 	//lettura di caratteri nel file di input 
-	while ((ch = getc(filein)) != EOF)
+	while (ch = getc(filein))
 	{
 		if (isdigit(ch))
 		{
@@ -49,55 +76,101 @@ int main(int argc, char* argv[]) {
 			if (dim_2 == 1)
 			{
 				value[0] = ch;
+				dim_2++;
 			}
 			else
 			{
 				// ri allochiamo 
-				values = realloc(values, (dim_2 + 1) * sizeof(int));
+				value = realloc(value, (dim_2));
 
-				value[dim_2] = ch;
+				value[dim_2 - 1] = ch;
 				dim_2++;
 			}
+			continue;
 		}
 		if (ch == '-')
 		{
-			value[dim_2] = ch;
+			value = realloc(value, (dim_2));
+			value[dim_2 - 1] = ch;
 			dim_2++;
+			continue;
 		}
-		if ((ch != '\n')||(ch != ' '))
-		{
-			break;
-			// incontrato un valore non accettabile
-		}
-		else
+		if ((ch == '\n') || (ch == ' ') || (ch == EOF))
 		{
 			// salviamo il valore trovato nell'array 
-			values = realloc(values, (dim_2 + 1) * sizeof(int));
-			value[dim_2] = '\0';
+			value = realloc(value, (dim_2));
+			value[dim_2 - 1] = '\0';
 
 			sscanf(value, "%d", &num);
+
 			if (dim == 1)
 			{
-				values[0] = value;
+				values[0] = num;
+				dim++;
 			}
 			else
 			{
 				// ri allochiamo 
-				values = realloc(values, (dim + 1) * sizeof(int));
-				values[dim] = value;
+				values = realloc(values, (dim) * sizeof(int));
+				values[dim - 1] = num;
 				dim++;
 
-				// azzeriamo l'indice di valu
-				dim_2 = 1;
 			}
+			// azzeriamo l'indice di value5
+			dim_2 = 1;
+			value = realloc(value, (dim_2));
+
+			if (ch == EOF)
+			{
+				break;
+			}
+
+		}
+		else
+		{
+			value = realloc(value, (dim_2));
+			value[dim_2 - 1] = '\0';
+
+			sscanf(value, "%d", &num);
+			// ri allochiamo 
+			values = realloc(values, (dim) * sizeof(int));
+			values[dim - 1] = num;
+			dim++;
+
+			break;
+			// incontrato un valore non accettabile
+
 		}
 
 	}
+	// sistemiamo la dimensione
+	dim--;
 
-	for (int i = 0; i < dim; i++)
+	//Bubble(dim,values);
+
+
+	int i;
+	bool ordinato = false;
+	size_t real = dim;
+	while (dim > 1 && !ordinato)
 	{
-		printf("%d\n", values[i]);
+		ordinato = true;
+		for (i = 0; i < dim - 1; i++)
+			if (values[i] > values[i + 1]) {
+				Scambia(&values[i], &values[i + 1]);
+				ordinato = false;
+			}
+		dim--;
 	}
+
+	for (int i = 0; i < real; i++)
+	{
+		fprintf(fileout, "%d\n", values[i]);
+	}
+
+	t2 = clock();
+
+	printf("tempo di esecuzione: %.10f sec\n", (t2 - t1) / (double)(CLOCKS_PER_SEC));
 
 	fclose(filein);
 	fclose(fileout);
