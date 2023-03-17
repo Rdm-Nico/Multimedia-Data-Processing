b4b46ad4ceecf757286bef789bb240f7028dba3b
nicorossi2810 <272179@studenti.unimore.it>
2023-03-17 08:51:47 +0100

    add the 4 ex of the assignment

---
+++ Second_assignment_v1/write_int11/Ex_4.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<fstream>
+#include<iterator>
+#include<vector>
+#include <bitset>
+
+template<typename T>
+std::ostream& raw_write(std::ostream& os, const T& val, size_t size = sizeof(T)) {
+    return os.write(reinterpret_cast<const char*>(&val), size);
+}
+
+int main(int argc, char* argv[]) {
+    
+    if (argc != 3) {
+        std::cout << "Usage: sort_int <filein.txt> <fileout.txt>\n";
+        return 1;
+    }
+    // open the files
+    std::ifstream is(argv[1]);
+    std::ofstream out(argv[2], std::ios::binary);
+
+    if (!is || !out)
+    {
+        std::cout << "Error int the files\n";
+        return 1;
+    }
+    // create the iterator 
+    std::istream_iterator<int32_t> is_start(is);
+    std::istream_iterator<int32_t> is_stop;
+
+    // init the vector with the all values 
+    std::vector<int32_t> v(is_start, is_stop);
+
+    std::vector<std::bitset<11>> b; 
+
+    for (size_t t = 0; t < v.size(); ++t)
+    {
+            std::bitset<11> b1(v[t]);
+            b.push_back(b1);
+    }
+    
+    uint8_t buf = 0;
+    uint8_t bit;
+    int num = 0; // numeri di bit segnati che ci sono nel byte attuale  
+    for (size_t t = 0; t < b.size(); t++)
+    {
+        size_t u = b[t].size()-1;
+        while (1)
+        {
+            bit = b[t][u];
+            buf = buf * 2 + bit;
+            // incrementiamo num
+            num++;
+
+            if (num == 8)
+            {
+                // stampiamo il valore  nel file
+                raw_write(out,buf);
+                // inseriamo i bit rimasti
+                num = 0;
+                buf = 0;
+                u--;
+                while(1)
+                {
+                    bit = b[t][u];
+                    buf = buf * 2 + bit;
+                    num++;
+                    if (u == 0)
+                    {
+                        break;
+                    }
+                    u--;
+                }
+                break;
+            }
+            else
+                // decrementiamo u
+                u--;
+        }
+
+    }
+
+	return 0;
+}
