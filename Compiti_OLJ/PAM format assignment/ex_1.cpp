1f0524fa374e8bf6044de58eab040657cdeb0570
Nico-Uni <272179@unimore.it>
2023-05-11 09:22:13 -0700

    add the new file and some corrections

--- Compiti_OLJ/PAM format assignment/ex_1.cpp
+++ Compiti_OLJ/PAM format assignment/ex_1.cpp
@@ -17,20 +17,20 @@ struct mat {
 	std::vector<T> data_; // matrice di int in questo caso 
 
 	// costruttore 
-	mat(int rows = 0, int cols = 0): rows_(rows), cols_(cols), data_(rows*cols){}
+	mat(int rows = 0, int cols = 0) : rows_(rows), cols_(cols), data_(rows* cols) {}
 
 	void resize(int rows, int cols) {
 		*this = mat(rows, cols); // operatore di assegnamento di defalut 
 	}
 
 	// prendere un elemento  in una certa riga e certa colonna
-	const T& operator()(int r, int c) const{
-		assert(r >= 0 && r < rows_ && c>= 0 && c < cols_);
+	const T& operator()(int r, int c) const {
+		assert(r >= 0 && r < rows_ && c >= 0 && c < cols_);
 		return data_[r * cols_ + c];
 	}
 
-	T& operator()(int r, int c){
-		assert(r >= 0 && r < rows_ && c>= 0 && c < cols_);
+	T& operator()(int r, int c) {
+		assert(r >= 0 && r < rows_ && c >= 0 && c < cols_);
 		return data_[r * cols_ + c];
 	}
 
@@ -39,23 +39,22 @@ struct mat {
 		//return const_cast<T&>(static_cast<const mat*>(this)->at(r, c));
 	//}
 
-	int rows() const { return rows_;}
-	int cols() const {return cols_;}
+	int rows() const { return rows_; }
+	int cols() const { return cols_; }
 	int size() const { return rows_ * cols_; }
 
 	// dimensione in byte dei dati
 	size_t raw_size()const {
 		return rows_ * cols_ * sizeof(T);
 	}
-	 // adesso facciamoo un cast per vedere i dati come un array di char
-	const char *raw_data()const { // per non fare copie perché non vogliamo modificare i dati
+	const char *raw_data()const {
 		return reinterpret_cast<const char *>(&data_[0]);
 	}
 };
 // sempre una const & perch� vogliamo non vogliamo una  copia 
 bool save_pam(const mat<uint8_t>& img, const std::string& filename) { // guarda se vuoi string view C++ 17
 	// create the PAM file 
-	std::ofstream out(filename,std::ios::binary); // per non tradurre i \n 
+	std::ofstream out(filename, std::ios::binary); // per non tradurre i \n 
 	if (!out) {
 		return false;
 	}
@@ -76,7 +75,7 @@ bool save_pam(const mat<uint8_t>& img, const std::string& filename) { // guarda 
 		{
 			out.put(img(r, c)); // put scrive un byte 
 		}
-	}	
+	}
 	// 2 metodo:
 	//out.write(img.raw_data(), img.raw_size());
 
@@ -90,7 +89,7 @@ template<typename T>
 void flip_inplace(mat<T>& img) {
 	using std::swap;
 	auto rows = img.rows();
-	for (int r = 0; r < rows /2; ++r)
+	for (int r = 0; r < rows / 2; ++r)
 	{
 		for (int c = 0; c < img.cols(); ++c)
 		{
@@ -100,7 +99,7 @@ void flip_inplace(mat<T>& img) {
 }
 
 // per l'img a colori RGB
-using rgb = std::array<uint8_t, 3>; // array di 3 byte
+using rgb = std::array<uint8_t, 3 >; // array di 3 byte
 
 
 bool save_pam(const mat<rgb>& img, const std::string& filename) { // guarda se vuoi string view C++ 17
@@ -120,32 +119,32 @@ bool save_pam(const mat<rgb>& img, const std::string& filename) { // guarda se v
 
 	// write the raster --> scriviamo tutti i byte della nostra immagine 
 	// 1 metodo:
-	for (int r = 0; r < img.rows(); ++r)
-	{
-		for (int c = 0; c < img.cols(); ++c)
-		{
-			out.put(img(r, c)[0]);
-			out.put(img(r, c)[1]);
-			out.put(img(r, c)[2]);// put scrive un byte 
-		}
-	}
+	//for (int r = 0; r < img.rows(); ++r)
+	//{
+	//	for (int c = 0; c < img.cols(); ++c)
+	//	{
+	//		out.put(img(r, c)[0]);
+	//		out.put(img(r, c)[1]);
+	//		out.put(img(r, c)[2]);// put scrive un byte 
+	//	}
+	//}
 	// 2 metodo:
 	//out.write(img.raw_data(), img.raw_size());
 
 	return true;
 }
 
-bool load_pam(mat<rgb> &img, const std::string& filename) {
+bool load_pam(mat<rgb>& img, const std::string& filename) {
 	std::ifstream is(filename, std::ios::binary);
 	if (!is)
 		return false;
 
-	// dentro string c'� getline
+	// dentro string c'ï¿½ getline
 	std:: string magic_number;
 	std::getline(is, magic_number);
 	// fino a end token ci sono le varie righe  -> che possono essere inserite in ordine causale 
 	// dobbiamo fare un parser -> che legge le righe e le insrisce bene 
-	
+
 	int w, h; // larghezza e altgezz a
 	while (1)
 	{
@@ -160,22 +159,22 @@ bool load_pam(mat<rgb> &img, const std::string& filename) {
 		if (token == "WIDTH") {
 			ss >> w;
 		}
-		else if(token == "EIGHT"){
+		else if (token == "EIGHT") {
 			ss >> h;
 		}
-		else if (token == "DEPTH"){
+		else if (token == "DEPTH") {
 			int depth;
 			ss >> depth;
 			if (depth != 1) // perchè abbiamo solo immagini a 1 canale
 				return false;
 		}
-		else if (token == "MAXVAL"){
+		else if (token == "MAXVAL") {
 			int  maxval;
-				ss >> maxval; 
-				if (maxval > 255)
-					return false;
+			ss >> maxval;
+			if (maxval > 255)
+				return false;
 		}
-		else if (token == "TUPLTYPE"){
+		else if (token == "TUPLTYPE") {
 			std::string tupltype;
 			ss >> tupltype;
 		}
@@ -189,14 +188,14 @@ bool load_pam(mat<rgb> &img, const std::string& filename) {
 	{
 		for (int c = 0; c < img.cols(); ++c)
 		{
-			img(r, c) = is.get(); // put permette di scrivere un byte
+			//img(r, c) = is.get(); // put permette di scrivere un byte
 		}
 	}
 
 	return true;
 }
 int main(void) {
-	
+
 
 	// create the matrix
 	mat<uint8_t> imgl(256, 256);
@@ -213,7 +212,7 @@ int main(void) {
 	// 2 ex 
 	mat<uint8_t> img2;
 	// read a file in pam version 
-	load_pam(img2, "frog.pam");
+	/*load_pam(img2, "frog.pam");*/
 
 	save_pam(img2, "frog_flipped.pam");
 
---
+++ Compiti_OLJ/PAM format assignment/sol_clean.cpp
@@ -0,0 +1,182 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<cassert>
+#include<vector>
+
+// this is a better and cleaned soluction for the ex_1, using the classes  that the prof has create
+/*the first assignment is:
+* 
+*	write a program that generates a gray level image of 256x256 pixels, where the first row is made of 256 zeros, second is 256 ones, ...
+* Save the img in PAM format. The img in XnView should appear as gradient black to white from top to bottom
+*  */
+
+
+// thing the img like a vector 
+template <typename T>
+struct mat
+{
+	int rows_;
+	int cols_;
+	std::vector<T> data_;
+
+	mat(int rows = 0, int cols = 0) : rows_(rows), cols_(cols), data_(rows* cols) {}
+
+	// is necessary to create a method to resize the dimension 
+	void resize(int rows, int cols) {
+		*this = mat(rows, cols); 
+	}
+	// create the overload operator: 
+	const T& operator()(int r, int c) const {
+		// make an assert 
+		assert(r >= 0 && r <= rows_ && c >= 0 && c <= cols_);
+		return data_[r * cols_ + c];
+	}
+	// also for change the value of a arbitrary number:
+	T& operator()(int r, int c) {
+		assert(r >= 0 && r <= rows_ && c >= 0 && c <= cols_);
+		return data_[r * cols_ + c];
+	}
+
+	// the other method for the selection:
+	int rows() const { return rows_; }
+	int cols() const { return cols_; }
+	int size() const { return cols_ * rows_; }
+
+	// dimension in byte of the data:
+	size_t raw_size() const {
+		return rows_ * cols_ * sizeof(T);
+	}
+	 // for saw the data like an array of char , we do  a casting 
+	const char* raw_data() const {
+		return reinterpret_cast<const char*>(&data_[0]);
+	}
+};
+
+
+// create a function con save a matrix in a file  in PAM format 
+bool save_pam(const mat<uint8_t>& img, const std::string& filename) {
+	// use ofstream method for don't translet the '\n' 
+	std::ofstream out(filename, std::ios::binary);
+	if (!out)
+		return false;
+
+	// insert the header of the pam:
+	out << "P7\n";
+	out << "WIDTH " << img.cols() << "\n";
+	out << "HEIGHT " << img.rows() << "\n";
+	out << "DEPTH 1\n";
+	out << "MAXVAL 255\n";
+	out << "TUPLTYPE GRAYSCALE\n";
+	out << "ENDHDR\n";
+
+	// now we can add all the data
+	for (size_t r = 0; r < img.rows(); ++r)
+	{
+		for (size_t c = 0; c < img.cols(); ++c)
+		{
+			out.put(img(r, c)); // put write one byte 
+		}
+	}
+	// second method:
+	//out.write(img.raw_data(), img.raw_size());
+
+	return true;
+}
+// we do the load pam of an image in grayscale
+bool load_pam(mat<uint8_t>& img, const std::string& filename)
+{
+	std::ifstream is(filename, std::ios::binary);
+	if (!is)
+	{
+		return false;
+	}
+	// now we do the parsing:
+	std::string magic_number;
+	std::getline(is, magic_number);
+	if (magic_number != "P7") {
+		return false;
+	}
+	int w, h;
+
+	// it's necessary to do a parse for all the rows 
+	while (1) {
+		std::string line;
+		std::getline(is, line);
+		if (line == "ENDHDR") {
+			break;
+		}
+		std::stringstream ss(line);
+		std::string token;
+		ss >> token;
+		if (token == "WIDTH") {
+			ss >> w;
+		}
+		else if (token == "HEIGHT") {
+			ss >> h;
+		}
+		else if (token == "DEPTH") {
+			int depth;
+			ss >> depth;
+			if (depth != 1) {
+				return false;
+			}
+		}
+		else if (token == "MAXVAL") {
+			int maxval;
+			ss >> maxval;
+			if (maxval != 255) {
+				return false;
+			}
+		}
+		else if (token == "TUPLETYPE") {
+			std::string tuple;
+			ss >> tuple;
+			if (tuple != "GRAYSCALE")
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	// resize the img 
+	img.resize(h, w);
+
+	// and fill the img with the rest of the data in the file:
+	for (size_t r = 0; r < img.rows(); ++r)
+	{
+		for (size_t c = 0; c < img.cols(); ++c)
+		{
+			img(r, c) = is.get();
+		}
+	}
+	return true;
+}
+
+int main(void) {
+
+	// create the matrix
+	mat<uint8_t> img1(256, 256);
+	
+	// we do the assignment of the data, following the text
+	for (size_t r = 0; r < img1.rows(); ++r)
+	{
+		for (size_t c = 0; c < img1.cols(); ++c)
+		{
+			img1(r, c) = r;
+		}
+	}
+	// know that we've all the data, we can save in PAM format
+	save_pam(img1, "out_clean.pam");
+
+	// 2 ex:
+	// is necessary to create a function that load a PAM file:
+
+
+	return 1;
+}
