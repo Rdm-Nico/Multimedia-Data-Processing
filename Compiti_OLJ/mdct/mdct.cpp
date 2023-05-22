4b00e436fa38d43696e038da6e163faea5176e89
Nico-Uni <272179@unimore.it>
2023-05-22 12:25:50 -0700

    add the mdct file

---
+++ Compiti_OLJ/mdct/mdct.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstdint>
+#include<vector>
+#include<iterator>
+#include<math.h>
+#include<string>
+#include<fstream>
+#include<istream>
+
+using namespace std;
+
+double entropy(vector<int16_t>& v) {
+	// calcoliamo l'entropia
+}
+
+int main(int argc, char* argv[]) {
+
+	// estrarre il file 
+
+	ifstream is("test.raw", std::ios::binary);
+
+	if (!is) {
+		cout << "error in the opening of the file\n";
+	}
+	istream_iterator<int16_t> is_start(is);
+	istream_iterator<int16_t> stop;
+
+	vector<int16_t> v(is_start, stop);
+
+	cout << "the size of v is: " << v.size();
+
+	double entropy = entropy(v);
+
+
+	return 0;
+}
