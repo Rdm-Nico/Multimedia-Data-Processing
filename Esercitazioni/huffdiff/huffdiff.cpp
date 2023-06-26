d4c1d6888d2b15b82be4b6fd3236e5970a3e34a7
Nico-Uni <272179@unimore.it>
2023-06-26 21:39:18 +0200

    changes

--- Esercitazioni/huffdiff/huffdiff.cpp
+++ Esercitazioni/huffdiff/huffdiff.cpp
@@ -9,17 +9,19 @@
 #include<cassert>
 #include<string>
 #include<sstream>
+#include "huffman.h"
+#include<unordered_map>
 using namespace std;
 
 
 
 
 template< typename T>
-class mat {
+struct mat {
 	int rows_;
 	int cols_;
 	vector<T> data_;
-public:
+
 	mat(int rows = 0, int cols = 0): rows_(rows), cols_(cols), data_(cols*rows){}
 
 	void resize(int rows, int cols) {
@@ -36,12 +38,12 @@ public:
 		return data_[r * cols_ + c];
 	}
 
-	int rows() { return rows_; }
-	int cols() { return cols_; }
-	int size() { return rows_*cols_; }
+	int rows() const { return rows_; }
+	int cols() const  { return cols_; }
+	int size() const { return rows_*cols_; }
 
-	size_t raw_size() {
-		return (rows_ * cols_ * sizeof(T));
+	size_t raw_size() const {
+		return rows_ * cols_ * sizeof(T);
 	}
 	char* raw_data() {
 		return reinterpret_cast<char*>(&data_[0]);
@@ -92,6 +94,51 @@ public:
 
 	explicit operator bool() const {
 		return !fail();
+	}
+};
+
+template<typename T>
+ostream& raw_write(ostream& os,const T& val, size_t size = sizeof(T)) {
+	return os.write(reinterpret_cast< const char*>(&val), size);
+}
+
+class bitwriter {
+	uint8_t buffer_;
+	int n_ = 0;
+	ostream& os_;
+
+	ostream& write_bit(uint8_t bit) {
+		
+		buffer_ = (buffer_ << 1) | bit;
+		n_++;
+		if (n_ == 8) {
+			raw_write(os_, buffer_);
+			n_ = 0;
+		}
+		return os_;
+	}
+public:
+	bitwriter(ostream& os): os_(os){}
+
+	ostream& write(uint32_t num, uint8_t n_bit) {
+		for (size_t i = n_bit - 1; i >= 0; --i) {
+			uint8_t cure_bit = (num >> i) & 1;
+			write_bit(cure_bit);
+		}
+		return os_;
+	}
+	ostream& operator()(uint32_t num, uint8_t n_bit) {
+		return write(num, n_bit);
+	}
+	~bitwriter() {
+		flush();
+	}
+	ostream& flush() {
+		while (n_ > 0)
+		{
+			write_bit(0);
+		}
+		return os_;
 	}
 };
 
@@ -174,23 +221,27 @@ bool save_pam(const string& filename,const  mat<uint8_t>& img) {
 
 	os.write(img.raw_data(), img.raw_size());
 
+	return true;
 }
 
 
 
 mat<int> calc_diffm(mat<uint8_t>& m) {
-	int prev = 0;
 
 	mat<int> diff;
 	diff.resize(m.rows(), m.cols());
 
-	for (int r = 0; r <= m.rows(); ++r) {
-		for (int c = 0; c <= m.cols(); ++c) {
+	int prev = 0;
+
+	for (int r = 0; r < m.rows(); ++r) {
+		for (int c = 0; c < m.cols(); ++c) {
 			diff(r, c) = m(r, c) - prev;
 			prev = m(r, c);
 		}
 		prev = m(r, 0);
 	}
+
+
 	return diff;
 }
 
@@ -203,57 +254,198 @@ mat<uint8_t> calc_viewable_diffm(mat<int>& diffm) {
 
 			if (rc == 0) {
 				v(r, c) = 128;
+				continue;
 			}
+
 			if (rc < -127) {
 				v(r, c) = 0;
 				continue;
 			}
+
 			if (rc > 127) {
 				v(r, c) = 255;
 				continue;
 			}
 
 			rc += 128;
+
 			v(r, c) = static_cast<uint8_t>(rc);
 		}
 	}
+
 	return v;
 }
 
+
+
+bool generate_huffdiff(mat<int> mdiff, const string out_filename, huffman<int>& h) {
+	ofstream os(out_filename, ios::binary);
+	if (!os)
+		return false;
+
+	bitwriter bw(os);
+
+	string magi_number = "HUFFDIFF";
+	os << magi_number;
+
+	uint32_t c = mdiff.cols();
+	uint32_t r = mdiff.rows();
+
+	// cosi siamo  sicuri che stampa di fila tutti i valori senza aggiungere whitespaces
+	raw_write(os, c);
+	raw_write(os, r);
+
+	// scrivo i numeri di elem della Table entries
+	bw.write(h.codes_table_.size(), 9);
+
+	// scriviamo la table entries
+	for (const auto& e : h.codes_table_) {
+		bw(e.sym_, 9);
+		bw(e.len_, 5);
+	}
+
+	// codifichiamo seguendo la tabella 
+	for (size_t r = 0; r < mdiff.rows(); r++) {
+		for (size_t c = 0; c < mdiff.cols(); c++) {
+			uint32_t val = 0, len = 0;
+			bool flag = false;
+
+			for (const auto& e : h.codes_table_) {
+				if (e.sym_ == mdiff(r, c)) {
+					val = e.val_;
+					len = e.len_;
+					flag = true;
+					break;
+				}
+			}
+
+			if (flag) { // se abbiamo trovato un elemento
+				bw.write(val, len); // scriviamo il valore del simbolo  con la sua lunghezza
+			}
+			else {
+				cout << "error " << " " << mdiff(r, c) << endl;
+			}
+		}
+	}
+	return true;
+
+}
+
+
 bool compressed(const string& file_in, const string& file_out){
 
-	mat<uint8_t> img;
+	mat<uint8_t> m;
 
-	if (!load_pam(file_in, img)) {
+	load_pam(file_in, m);
+	/*
+	if (!load_pam(file_in, m)) {
 		cout << "error in load the pam file" << endl;
 		return false;
 	}
-	mat<int> mdiff = calc_diffm(img);
+	*/
+	mat<int> mdiff = calc_diffm(m);
 
+	/*
 	mat<uint8_t> viewable_mdiff = calc_viewable_diffm(mdiff);
 	if (!save_pam("diff.pam", viewable_mdiff)) {
 		cout << "error in saving the pam file..." << endl;
 		return false;
 	}
-
-
-	/*
-	huffdiff <uint16_t,uint16_t>hf(img);
-
-	hf.compute_diff_mat();
-	hf.compute_freq();
-	hf.create_table();
-	hf.compute_canonical_codes();
-
 	*/
-	bitreader br(is);
 
+	huffman<int> h;
+
+	unordered_map<int, uint32_t> freq;
+
+	for (size_t r = 0; r < mdiff.rows(); r++) {
+		for (size_t c = 0; c < mdiff.cols(); c++) {
+			int f = mdiff(r, c);
+			freq[f]++;
+		}
+	}
+
+	h.compute_codes_table(freq);
+	h.compute_canonical_codes();
+
+	generate_huffdiff(mdiff, file_out, h);
+	
 
 	return true;
 }
 
+void error(string) {
+	return;
+}
 bool decompressed(const string& file_in, const string& file_out) {
 
+	ifstream is(file_in, ios::binary);
+	if (!is) {
+		return false;
+	}
+
+	string magic_number(8,' ');
+	raw_read(is, magic_number[0], 8);
+	if (magic_number != "HUFFDIFF") {
+		error("wrong input format\n");
+	}
+	 
+	int width, height;
+	raw_read(is, width);
+	raw_read(is, height);
+
+	bitreader br(is);
+	uint32_t tmp;
+
+	tmp = br.read(9);
+	size_t TableEntries = tmp;
+
+	huffman<int> h;
+
+	for (size_t i = 0; i < TableEntries; ++i) {
+		huffman<int>::code t; // creiamo un elemento code
+		t.sym_ = br.read(9);
+		t.len_ = br.read(5);
+		h.codes_table_.push_back(t);
+	}
+
+	h.compute_canonical_codes();
+
+	mat<int> diff(height, width);
+
+	for (int r = 0; r < diff.rows(); ++r) {
+		for (int c = 0; c < diff.cols(); ++c) {
+			uint32_t len = 0, code = 0;
+			size_t pos = 0;
+			do {
+				while (h.codes_table_[pos].len_ > len) {
+					uint32_t bit;
+					bit = br.read_bit();
+					code = (code << 1) | bit; 
+					++len;
+				}
+				if (code == h.codes_table_[pos].val_) {
+					break;
+				}
+				++pos;
+			} while (pos < h.codes_table_.size());
+			if (pos == h.codes_table_.size()) {
+				error("questo non dovrebbe accadere\n");
+			}
+			diff(r, c) = h.codes_table_[pos].sym_;
+		}
+	}
+
+	mat<uint8_t> img(diff.rows(), diff.cols());
+
+	int prev = 0;
+	for (int r = 0; r < diff.rows(); ++r) {
+		for (int c = 0; c < diff.cols(); ++c) {
+			img(r, c) = diff(r, c) + prev;
+			prev = img(r, c);
+		}
+		prev = img(r, 0);
+	}
+	save_pam(file_out, img);
 	return true;
 }
 
